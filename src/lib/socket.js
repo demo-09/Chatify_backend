b0f8cfe8c367221f7a04698a1ddaecca7d380c2f
raj.vadukul <raj.vadukul@prudentcorporate.com>
2026-05-08 16:01:30 +0530

    fix cors

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -3,35 +3,45 @@ import http from "http";
 import express from "express";
 
 const app = express();
+
 const server = http.createServer(app);
 
 const io = new Server(server, {
-  cors: {
-    origin: ["http://localhost:5173"],
-  },
+    cors: {
+        origin: [
+            "http://localhost:5173",
+            "https://chatify-frontend-sigma.vercel.app",
+        ],
+        credentials: true,
+    },
 });
 
-export function getReceiverSocketId(userId) {
-  return userSocketMap[userId];
-}
+export const getReceiverSocketId = (userId) => {
+    return userSocketMap[userId];
+};
 
-// used to store online users
-const userSocketMap = {}; // {userId: socketId}
+const userSocketMap = {};
 
 io.on("connection", (socket) => {
-  console.log("A user connected", socket.id);
+    console.log("User connected:", socket.id);
 
-  const userId = socket.handshake.query.userId;
-  if (userId) userSocketMap[userId] = socket.id;
+    const userId = socket.handshake.query.userId;
 
-  // io.emit() is used to send events to all the connected clients
-  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+    console.log("Connected userId:", userId);
 
-  socket.on("disconnect", () => {
-    console.log("A user disconnected", socket.id);
-    delete userSocketMap[userId];
+    if (userId) {
+        userSocketMap[userId] = socket.id;
+    }
+
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
-  });
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected:", socket.id);
+
+        delete userSocketMap[userId];
+
+        io.emit("getOnlineUsers", Object.keys(userSocketMap));
+    });
 });
 
 export { io, app, server };
