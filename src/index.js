87d07275d787308bcd2ca0c111a91b99bc5422e5
raj.vadukul <raj.vadukul@prudentcorporate.com>
2026-05-08 15:29:16 +0530

    new

--- src/index.js
+++ src/index.js
@@ -2,58 +2,46 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+
+
+
 import path from "path";
 
 import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-
 import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5001;
-
+const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-
-// Middleware
 app.use(express.json());
 app.use(cookieParser());
-
 app.use(
     cors({
-        origin: true,
+        origin:
+            process.env.NODE_ENV === "development"
+                ? "http://localhost:5173"
+                : "https://your-frontend.vercel.app",
+
         credentials: true,
     })
 );
-
-
-// API Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-
-// Frontend Serve
 if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-    app.get("*", (req, res) => {
-        res.sendFile(
-            path.join(__dirname, "../frontend/dist/index.html")
-        );
-    });
-
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
-
-// Start Server
 server.listen(PORT, () => {
-
-    console.log("Server is running on PORT:", PORT);
-
-    connectDB();
-
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
 });
